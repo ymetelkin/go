bf9a57e0c171eb76e35a557026e41d7e1376ef15
Yuri Metelkin <ymetelkin@yahoo.com>
2019-05-06 00:07:38 -0400

    Work in progress

--- apql/sets.go
+++ apql/sets.go
@@ -3,6 +3,7 @@ package apql
 import (
 	"errors"
 	"fmt"
+	"strings"
 )
 
 type set struct {
@@ -26,24 +27,21 @@ type bools struct {
 	Bools   map[int]bools
 }
 
-func booleanize(tokens []token) (bools, error) {
-	if tokens == nil {
-		return bools{}, errors.New("No input tokens found")
-	}
-
-	size := len(tokens)
-	if size == 0 {
-		return bools{}, errors.New("No input tokens found")
+func booleanize(tokens []token) (set, error) {
+	if tokens == nil || len(tokens) == 0 {
+		return set{}, errors.New("No input tokens found")
 	}
 
 	var (
 		txt, op string
+		idx     int
 	)
 
-	qs := make([]query, 0)
+	queries := make(map[int]query)
+	sets := make(map[int]set)
+	ors := make([]int, 0)
 	ands := make([]int, 0)
 	nots := make([]int, 0)
-	bls := make(map[int]bools)
 
 	for _, tok := range tokens {
 		switch tok.Type {
@@ -55,7 +53,8 @@ func booleanize(tokens []token) (bools, error) {
 						Operator: op,
 						Value:    tok.Text,
 					}
-					qs = append(qs, q)
+					queries[idx] = q
+					idx++
 					txt = ""
 					op = ""
 				} else {
@@ -64,7 +63,8 @@ func booleanize(tokens []token) (bools, error) {
 						Operator: "=",
 						Value:    txt,
 					}
-					qs = append(qs, q)
+					queries[idx] = q
+					idx++
 					txt = tok.Text
 				}
 			} else {
@@ -75,10 +75,10 @@ func booleanize(tokens []token) (bools, error) {
 				if tok.Text == "=" {
 					op += "="
 				} else {
-					return bools{}, fmt.Errorf("Expected '=', found '%s'", tok.Text)
+					return set{}, fmt.Errorf("Expected '=', found '%s'", tok.Text)
 				}
 			} else if op != "" {
-				return bools{}, fmt.Errorf("Expected '%s', found '%s%s'", op, op, tok.Text)
+				return set{}, fmt.Errorf("Expected '%s', found '%s%s'", op, op, tok.Text)
 			} else {
 				op = tok.Text
 			}
@@ -87,22 +87,23 @@ func booleanize(tokens []token) (bools, error) {
 				if op == "HAS" || op == "HASANY" || op == "HASALL" {
 					vals, err := getValues(tok.Tokens)
 					if err != nil {
-						return bools{}, err
+						return set{}, err
 					}
 					q := query{
 						Field:    txt,
 						Operator: op,
 						Values:   vals,
 					}
-					qs = append(qs, q)
+					queries[idx] = q
+					idx++
 					txt = ""
 					op = ""
 				} else if op != "" {
-					return bools{}, fmt.Errorf("Expected 'HAS', found '%s'", op)
+					return set{}, fmt.Errorf("Expected 'HAS', found '%s'", op)
 				} else {
-					bs, err := booleanize(tok.Tokens)
+					st, err := booleanize(tok.Tokens)
 					if err != nil {
-						return bools{}, err
+						return set{}, err
 					}
 
 					q := query{
@@ -110,32 +111,175 @@ func booleanize(tokens []token) (bools, error) {
 						Operator: "=",
 						Value:    txt,
 					}
-					qs = append(qs, q)
-					bls[len(qs)] = bs
+					queries[idx] = q
+					idx++
+					sets[idx] = st
+					idx++
 					txt = ""
 				}
 			} else {
-				bs, err := booleanize(tok.Tokens)
+				st, err := booleanize(tok.Tokens)
 				if err != nil {
-					return bools{}, err
+					return set{}, err
 				}
-				bls[len(qs)] = bs
+				sets[idx] = st
+				idx++
 			}
 		case tokenBool:
+			if op != "" {
+				return set{}, fmt.Errorf("Expected '%s', found '%s %s'", tok.Text, op, tok.Text)
+			}
+			if txt != "" {
+				q := query{
+					Field:    "_all",
+					Operator: "=",
+					Value:    txt,
+				}
+				queries[idx] = q
+				idx++
+				txt = ""
+			}
+
+			size := len(queries)
+			asz := len(ands)
+			osz := len(ors)
+			nsz := len(nots)
+
 			if tok.Text == "AND" {
-				ands = append(ands, len(qs))
+				if asz > 0 && ands[asz-1] == size {
+					return set{}, errors.New("Expected 'AND', found 'AND AND")
+				}
+				if osz > 0 && ors[osz-1] == size {
+					return set{}, errors.New("Expected 'AND', found 'OR AND")
+				}
+				if nsz > 0 && nots[nsz-1] == size {
+					return set{}, errors.New("Expected 'AND', found 'NOT AND")
+				}
+				ands = append(ands, size)
 			} else if tok.Text == "NOT" {
-				nots = append(nots, len(qs))
+				if nsz > 0 && nots[nsz-1] == size {
+					return set{}, errors.New("Expected 'NOT', found 'NOT NOT")
+				}
+				nots = append(nots, size)
+			} else if tok.Text == "OR" {
+				if asz > 0 && ands[asz-1] == size {
+					return set{}, errors.New("Expected 'OR', found 'AND OR")
+				}
+				if osz > 0 && ors[osz-1] == size {
+					return set{}, errors.New("Expected 'OR', found 'OR OR")
+				}
+				if nsz > 0 && nots[nsz-1] == size {
+					return set{}, errors.New("Expected 'OR', found 'NOT OR")
+				}
+				ors = append(ors, size)
 			}
 		}
 	}
 
-	return bools{
-		Queries: qs,
-		Ands:    ands,
-		Nots:    nots,
-		Bools:   bls,
-	}, nil
+	ret := set{}
+
+	if len(nots) > 0 {
+		for i := range nots {
+			st := set{Not: true}
+			if q, ok := queries[i]; ok {
+				st.Queries = []query{q}
+				delete(queries, i)
+			} else if s, ok := sets[i]; ok {
+				st.Sets = []set{s}
+				delete(sets, i)
+			} else {
+				return set{}, errors.New("Expected expression, found 'NOT'")
+			}
+
+			if ret.Sets == nil {
+				ret.Sets = []set{st}
+			} else {
+				ret.Sets = append(ret.Sets, st)
+			}
+		}
+	}
+
+	if len(ands) > 0 {
+		var (
+			last int
+			st   set
+		)
+
+		for _, i := range ands {
+			prev := last > 0 && last == i-1
+
+			if !prev {
+				if st.And {
+					if ret.Sets == nil {
+						ret.Sets = []set{st}
+					} else {
+						ret.Sets = append(ret.Sets, st)
+					}
+				}
+				st = set{And: true}
+			}
+
+			if !prev {
+				if q, ok := queries[i-1]; ok {
+					if st.Queries == nil {
+						st.Queries = []query{q}
+					} else {
+						st.Queries = append(st.Queries, q)
+					}
+					delete(queries, i-1)
+				} else if s, ok := sets[i-1]; ok {
+					if st.Sets == nil {
+						st.Sets = []set{s}
+					} else {
+						st.Sets = append(st.Sets, s)
+					}
+					delete(sets, i-1)
+				} else {
+					return set{}, errors.New("Expected expression, found 'AND'")
+				}
+			}
+
+			if q, ok := queries[i]; ok {
+				st.Queries = append(st.Queries, q)
+				delete(queries, i)
+				last = i
+			} else if s, ok := sets[i]; ok {
+				st.Sets = append(st.Sets, s)
+				delete(sets, i)
+				last = i
+			} else {
+				return set{}, errors.New("Expected expression, found 'AND'")
+			}
+		}
+
+		if st.And {
+			if ret.Sets == nil {
+				ret.Sets = []set{st}
+			} else {
+				ret.Sets = append(ret.Sets, st)
+			}
+		}
+	}
+
+	if len(queries) > 0 {
+		ret.Queries = make([]query, 0)
+		for _, q := range queries {
+			ret.Queries = append(ret.Queries, q)
+		}
+	}
+
+	if len(sets) > 0 {
+		ret.Sets = make([]set, 0)
+		for _, s := range sets {
+			ret.Sets = append(ret.Sets, s)
+		}
+	}
+
+	if (ret.Queries == nil || len(ret.Queries) == 0) && ret.Sets != nil && len(ret.Sets) == 1 {
+		return ret.Sets[0], nil
+	}
+
+	return ret, nil
 }
 
 func getValues(tokens []token) ([]string, error) {
@@ -159,4 +303,80 @@ func getValues(tokens []token) ([]string, error) {
 	}
 
 	return vals, nil
+}
+
+func (q *query) string() string {
+	var sb strings.Builder
+	if !strings.HasPrefix(q.Field, "@") {
+		sb.WriteString("@")
+	}
+	sb.WriteString(q.Field)
+	sb.WriteString(q.Operator)
+
+	if q.Value == "" {
+		sb.WriteString("(")
+		for i, v := range q.Values {
+			if i > 0 {
+				sb.WriteString(" ")
+			}
+			sb.WriteString(v)
+		}
+		sb.WriteString(")")
+	} else {
+		sb.WriteString(q.Value)
+	}
+
+	return sb.String()
+}
+
+func (st *set) string() string {
+	var (
+		sb strings.Builder
+		sp bool
+	)
+
+	if st.Not {
+		sb.WriteString("NOT (")
+
+		if st.Queries != nil && len(st.Queries) > 0 {
+			sb.WriteString(st.Queries[0].string())
+		} else if st.Sets != nil && len(st.Sets) > 0 {
+			sb.WriteString(st.Sets[0].string())
+		}
+
+		sb.WriteString(")")
+	} else {
+		b := " OR "
+		if st.And {
+			b = " AND "
+		}
+
+		if st.Queries != nil && len(st.Queries) > 0 {
+			for _, q := range st.Queries {
+				if sp {
+					sb.WriteString(b)
+				} else {
+					sp = true
+				}
+
+				sb.WriteString(q.string())
+			}
+		}
+
+		if st.Sets != nil && len(st.Sets) > 0 {
+			for _, s := range st.Sets {
+				if sp {
+					sb.WriteString(b)
+				} else {
+					sp = true
+				}
+
+				sb.WriteString("(")
+				sb.WriteString(s.string())
+				sb.WriteString(")")
+			}
+		}
+	}
+
+	return sb.String()
 }
