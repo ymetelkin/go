c4570ddf8c8e136e95f549e3a078e6c521133801
Yuri Metelkin <ymetelkin@yahoo.com>
2019-10-06 17:40:48 -0400

    Update utils.go

--- appl/utils.go
+++ appl/utils.go
@@ -34,7 +34,7 @@ func (us *uniqueStrings) Append(s string) {
 }
 
 func (us *uniqueStrings) IsEmpty() bool {
-	return us.values == nil || len(us.values) == 0
+	return us.values == nil
 }
 
 func (us *uniqueStrings) Values() []string {
@@ -92,7 +92,7 @@ func (us *uniqueCodeNames) Append(code string, name string) {
 }
 
 func (us *uniqueCodeNames) IsEmpty() bool {
-	return us.values == nil || len(us.values) == 0
+	return us.values == nil
 }
 
 func (us *uniqueCodeNames) Values() []CodeName {
@@ -111,99 +111,6 @@ func (us *uniqueCodeNames) JSONArray(f func(CodeName) json.Object) (ja json.Arra
 			ja.AddObject(jo)
 		}
 	}
-	return
-}
-
-type uniqueArray struct {
-	keys   map[string]bool
-	values json.Array
-}
-
-func (ua *uniqueArray) AddString(s string) {
-	if s == "" {
-		return
-	}
-
-	if ua.keys == nil {
-		ua.keys = make(map[string]bool)
-	} else {
-		_, ok := ua.keys[s]
-		if ok {
-			return
-		}
-	}
-
-	ua.keys[s] = true
-	ua.values.AddString(s)
-}
-
-func (ua *uniqueArray) AddKeyValue(kn string, kv string, vn string, vv string) {
-	if kv == "" || vv == "" {
-		return
-	}
-
-	key := fmt.Sprintf("%s_%s", kv, vv)
-	if ua.keys == nil {
-		ua.keys = make(map[string]bool)
-	} else {
-		_, ok := ua.keys[key]
-		if ok {
-			return
-		}
-	}
-
-	ua.keys[key] = true
-
-	jo := json.Object{}
-	jo.AddString(kn, kv)
-	jo.AddString(vn, vv)
-	ua.values.AddObject(jo)
-}
-
-func (ua *uniqueArray) AddObject(key string, jo json.Object) {
-	if jo.IsEmpty() {
-		return
-	}
-
-	if ua.keys == nil {
-		ua.keys = make(map[string]bool)
-	} else {
-		_, ok := ua.keys[key]
-		if ok {
-			return
-		}
-	}
-
-	ua.keys[key] = true
-	ua.values.AddObject(jo)
-}
-
-func (ua *uniqueArray) IsEmpty() bool {
-	return ua.values.Length() == 0
-}
-
-func (ua *uniqueArray) JSONProperty(field string) json.Property {
-	if ua.values.Length() == 0 {
-		return json.Property{}
-	}
-	return json.NewArrayProperty(field, ua.values)
-}
-
-func (ua *uniqueArray) JSONArray() json.Array {
-	return ua.values
-}
-
-func (ua *uniqueArray) Values() (values []string) {
-	if ua.keys == nil {
-		return
-	}
-
-	for k := range ua.keys {
-		values = append(values, k)
-	}
-
-	sort.Strings(values)
-
 	return
 }
 
