a2a8f5a314d4cd0be39d61613784cbfcbbc934ea
Yuri Metelkin <ymetelkin@yahoo.com>
2021-04-20 12:38:11 -0400

    cleanup

---
+++ json/v2/go.mod
@@ -0,0 +1,3 @@
+module github.com/ymetelkin/go/json/v2
+
+go 1.16
--- rest/response.go
+++
@@ -1,8 +0,0 @@
-package rest
-
-//Response is REST response
-type Response struct {
-	StatusCode int
-	Headers    map[string]string
-	Body       string
-}
